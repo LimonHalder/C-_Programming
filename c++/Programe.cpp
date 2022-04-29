9a686761723a50222530c4e4f864141eae9cd31a
Limon Halder <82777945+LimonHalder@users.noreply.github.com>
2022-04-29 13:27:25 +0600

    Add files via upload

---
+++ c++/Programe.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello in c++";
+    return 0;
+}
---
+++ c++/datatype.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+
+    int a;
+    cout<<"Size of int"<<sizeof(a)<<endl;
+
+    float b;
+
+    cout<<"Size of float"<<sizeof(b)<<endl;
+    char c;
+    cout<<"Size of char"<<sizeof(c)<<endl;
+    bool d;
+    cout<<"Size of bool"<<sizeof(d)<<endl;
+    long int e;
+    cout<<"Size of lonint"<<sizeof(e)<<endl;
+    short int f;
+    cout<<"Size of shortint"<<sizeof(f)<<endl;
+
+
+    return 0;
+}
